485949e2d4165dba68680a813cae7bd2a1ab304a
JohnReid <johnbaronreid@netscape.net>
2016-03-22 19:16:21 +0000

    Add analysis of variance.

---
+++ R/aov.r
@@ -0,0 +1,15 @@
+#' Perform an analysis of variance to select genes for the DeLorean model.
+#'
+#' @param dl DeLorean object.
+#'
+#' @export
+#'
+aov.dl <- function(dl) {
+  melt.expr(dl) %>%
+    dplyr::left_join(dl$cell.meta) %>%
+    dplyr::group_by(gene) %>%
+    dplyr::do(broom::tidy(aov(x ~ capture, .))) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(term == 'capture') %>%
+    dplyr::arrange(p.value)
+}
