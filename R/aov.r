fe23d8179eaf18a4ae129205300e653a9b842716
JohnReid <johnbaronreid@netscape.net>
2017-07-17 18:12:37 +0100

    Add argument to use custom expression data.

--- R/aov.r
+++ R/aov.r
@@ -1,11 +1,13 @@
 #' Perform an analysis of variance to select genes for the DeLorean model.
 #'
 #' @param dl DeLorean object.
+#' @param expr.l Melted (with melt.expr()) expression values to use
 #'
 #' @export
 #'
-aov.dl <- function(dl) {
-  dl$aov <- melt.expr(dl) %>%
+aov.dl <- function(dl, expr.l = melt.expr(dl)) {
+  dl$aov <-
+    expr.l %>%
     dplyr::left_join(dl$cell.meta) %>%
     dplyr::group_by(gene) %>%
     dplyr::do(broom::tidy(aov(x ~ capture, .))) %>%
